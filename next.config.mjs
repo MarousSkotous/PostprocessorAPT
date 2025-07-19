b703e1486b1ce8c517a532375163d32f4558de04
Marek Skoták <marekskotak@Mark-Book-Pro.local>
2025-07-19 21:41:05 +0200

    Disable SWC minify to see full React errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // vypne minifikaci, aby React házel neminifikované chyby
+  swcMinify: false,
 };
 
 export default nextConfig;
